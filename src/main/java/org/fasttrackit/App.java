2ea48e46cce052336adae0a770b1b7b1151cccb0
Roxana <roxanacroitoru378@yahoo.ro>
2021-01-25 12:51:57 +0000

    tema 15

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,13 +1,24 @@
 package org.fasttrackit;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        int x = 127;
+        int y = 323;
+
+        System.out.println("Calculating the sum of " + x + " and " + y);
+        int sum = calculator.sum(x, y);
+        System.out.println("The sum is " + sum);
+
+        System.out.println("Calculating the product of " + x + " and " + y);
+        int product = calculator.multiply(x, y);
+        System.out.println("The product is " + product);
+
+        System.out.println("Calculating the division of " + x + " and " + y);
+        double result = calculator.divide(x, y);
+        System.out.println("The division is " + result);
+
     }
 }
---
+++ src/main/java/org/fasttrackit/Calculator.java
@@ -0,0 +1,17 @@
+package org.fasttrackit;
+
+public class Calculator {
+
+    public int sum(int a, int b){
+        return a + b;
+    }
+
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
+    public double divide(double a, double b){
+        return a / b;
+    }
+
+}
